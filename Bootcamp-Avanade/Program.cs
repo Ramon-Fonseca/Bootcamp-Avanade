5051e0be7fa5aed7becb454c839d9dd9e69293c0
Ramon <ramon.fc17@gmail.com>
2025-09-11 21:14:44 -0300

    Primeiro desafio, ainda finalizando o codigo

--- Bootcamp-Avanade/Program.cs
+++ Bootcamp-Avanade/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        // Solicita a entrada de dados do usuário 
+        Console.WriteLine("Digite o tipo de serviço desejado (basico, intermediario, avancado ou premium): ");
+        string entrada = Console.ReadLine().ToUpper();
+
+        // Validação da entrada logo após o input
+        if (string.IsNullOrWhiteSpace(entrada))
+        {
+            Console.WriteLine("Serviço inválido");
+            return; // Encerra o programa
+        }
+    }
+}
